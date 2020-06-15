4f53e52e542b469eb4a87f25c7d289a868b337d9
yale <yinhuwang@gmail.com>
2020-06-15 17:08:50 +0800

    hehe

--- cut.py
+++ cut.py
@@ -17,12 +17,12 @@ im = Image.open(inFileName)
 
 if(im.size[0] != im.size[1]):
     exit("error! image not square")
+
 l = 400
 g = 10
 L = 3 * l + 2 * g
 
 region = im.resize((L, L))
-
 
 boxes = ((0, 0, l, l),
          (l+g, 0, 2*l+g, l),
@@ -36,6 +36,7 @@ boxes = ((0, 0, l, l),
          )
 
 os.makedirs(outDirName,exist_ok=True)
+
 for index, box in enumerate(iterable=boxes, start=1):
     out = region.crop(box)
     out.save(outDirName + "/" + str(index) + "." + fileType)
